ea85d6969cbc79013ae4a152ff759df76ce8e7e9
Adam <adamjpolak@gmail.com>
2025-02-14 14:13:23 -0600

    add auth0 to clientapp

--- ClientApp/src/components/Counter.js
+++ ClientApp/src/components/Counter.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import LogoutButton from './Logout';
 
 export class Counter extends Component {
   static displayName = Counter.name;
@@ -18,6 +19,7 @@ export class Counter extends Component {
   render() {
     return (
       <div>
+        <LogoutButton />
         <h1>Counter</h1>
 
         <p>This is a simple example of a React component.</p>
