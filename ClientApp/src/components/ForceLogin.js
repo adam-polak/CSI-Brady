fcc654d2d2d6277399d372ed075908c3e619f7bb
Adam <adamjpolak@gmail.com>
2025-02-17 09:08:16 -0600

    add route arg to forcelogin component

--- ClientApp/src/components/ForceLogin.js
+++ ClientApp/src/components/ForceLogin.js
@@ -1,12 +1,12 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom"
 
-export const ForceLogin = () => {
+export const ForceLogin = ({redirectRoute}) => {
     const { isAuthenticated, loginWithRedirect, isLoading } = useAuth0();
     const nav = useNavigate();
 
     if(isAuthenticated) {
-        nav("/counter");
+        nav(redirectRoute);
     } else if(!isLoading) {
         loginWithRedirect();
     }
