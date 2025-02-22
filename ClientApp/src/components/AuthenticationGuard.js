6bf514ef0ed9b9c214cafa4ee7a8492f9cb2b93c
Adam <adamjpolak@gmail.com>
2025-02-22 13:13:38 -0600

    fix redirecting when not logged in, and retstructure components

--- ClientApp/src/components/AuthenticationGuard.js
+++ ClientApp/src/components/AuthenticationGuard.js
@@ -1,14 +1,20 @@
 import { useAuth0, withAuthenticationRequired } from "@auth0/auth0-react";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthenticationGuard = ({ component }) => {
-    const { isAuthenticated } = useAuth0();
+    const { isAuthenticated, isLoading } = useAuth0();
+    const nav = useNavigate();
 
     const Component = withAuthenticationRequired(component, {
         onRedirecting: () => (
             <h2>Loading...</h2>
         )
     });
+
+    if(!isLoading && !isAuthenticated) {
+        nav("/");
+    }
 
     return isAuthenticated && <Component />
 }
