410e7cdaebef55bef8d02daf7d7c6add69da7a7a
Adam <adamjpolak@gmail.com>
2025-02-22 13:48:52 -0600

    styles

--- ClientApp/src/components/facility/FacilityEntry.js
+++ ClientApp/src/components/facility/FacilityEntry.js
@@ -1,15 +1,27 @@
 import React from "react";
+import { Button } from 'reactstrap';
 
-export default function FacilityEntry({ children, onSelectItem }) {
-  const style = {
-    borderStyle: "solid",
-    textAlign: "center",
-    margin: "10px",
-  };
+/**
+ * facility object
+ * {
+ * Address,
+ * CompanyName,
+ * CompanyImgLink
+ * }
+ */
+
+export default function FacilityEntry({ facility, onSelectItem }) {
+  // const style = {
+  //   borderStyle: "solid",
+  //   textAlign: "center",
+  //   margin: "10px",
+  // };
 
   return (
-    <div className="projectEntry" onClick={onSelectItem} style={style}>
-      {children}
+    <div>
+      <Button onClick={onSelectItem}>
+        test
+      </Button>
     </div>
   );
 }
