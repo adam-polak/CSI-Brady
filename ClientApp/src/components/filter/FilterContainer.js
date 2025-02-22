1e6163051eec7ef742bb5f923049b741d9b36520
NathanSt10 <nstout@uwm.edu>
2025-02-22 16:06:09 -0600

    Merge

---
+++ ClientApp/src/components/filter/FilterContainer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import FilterBar from './FilterBar'
+import FilterTag from './FilterTag'
+
+export default function FilterContainer() {
+  return (
+    <div><FilterContainer /></div>
+  )
+}
+
