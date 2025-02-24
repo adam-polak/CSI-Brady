b6efb08b3d450a2759529c08b238fe237cdf2380
Adam <adamjpolak@gmail.com>
2025-02-23 20:13:04 -0600

    filters working

--- ClientApp/src/components/facility/FacilitiesContainer.js
+++ ClientApp/src/components/facility/FacilitiesContainer.js
@@ -3,6 +3,37 @@ import { useNavigate } from "react-router-dom";
 import FacilityEntry from "./FacilityEntry";
 import FilterBar from "../filter/FilterBar";
 
+function FilteredFacilities({handleSelectEntry, facilities, filters}) {
+  const arr = facilities.filter((f) => {
+    if(filters.length === 0) {
+      return true;
+    }
+
+    const lowercaseFilters = filters.map(x => x.toLowerCase());
+
+    let ans = true;
+    const address = f.Address.toLowerCase();
+    const companyName = f.CompanyName.toLowerCase();
+    for(let i = 0; i < lowercaseFilters.length; i++) {
+      ans = ans && (address.includes(lowercaseFilters[i]) || companyName.includes(lowercaseFilters[i]));
+    }
+
+    return ans;
+  });
+
+  return (
+    <>
+      {arr.map((facility, i) => (
+            <FacilityEntry
+            key={`facility-${i}`}
+            facility={facility}
+            onSelectItem={() => handleSelectEntry(i)}
+          />
+      ))}
+    </>
+  );
+}
+
 export default function FacilitiesContainer({ facilities }) {
   const [filters, setFilters] = useState([]);
   const nav = useNavigate();
@@ -30,18 +61,7 @@ export default function FacilitiesContainer({ facilities }) {
         tags={filters}
       />
       <div className="container-lg mt-3">
-        {facilities
-          .filter(
-            (facility) =>
-              filters.length === 0 || filters.includes(facility.Address)
-          )
-          .map((facility, i) => (
-            <FacilityEntry
-              key={`facility-${i}`}
-              facility={facility}
-              onSelectItem={() => handleSelectEntry(i)}
-            />
-          ))}
+        <FilteredFacilities handleSelectEntry={handleSelectEntry} facilities={facilities} filters={filters} />
       </div>
     </>
   );
