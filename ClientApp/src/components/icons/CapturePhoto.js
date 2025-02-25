4a947a3ec1b10ce757dd59d751b2648ab6d8914b
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-02-25 14:22:40 -0600

    Change capture photo button

--- ClientApp/src/components/icons/CapturePhoto.js
+++ ClientApp/src/components/icons/CapturePhoto.js
@@ -2,17 +2,16 @@ import React from "react";
 const CapturePhoto = ({ size = 24, color = "#000000" }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
+    width={size}
+    height={size}
     viewBox="0 0 24 24"
     fill="none"
     stroke={color}
+    strokeWidth="2.5"
     strokeLinecap="round"
     strokeLinejoin="round"
-    width={size}
-    height={size}
-    strokeWidth="2"
   >
-    <path d="M12 21a9 9 0 0 0 9 -9a9 9 0 0 0 -9 -9a9 9 0 0 0 -9 9a9 9 0 0 0 9 9z"></path>
-    <path d="M12 12m-4.5 0a4.5 4.5 0 1 0 9 0a4.5 4.5 0 1 0 -9 0"></path>
+    <circle cx="12" cy="12" r="10"></circle>
   </svg>
 );
 export default CapturePhoto;
