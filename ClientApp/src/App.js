09a291874a9c3cc443a0e8b71ba5d522580ad0e6
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-02-25 23:06:07 -0600

    Header Bar with layout to all pages, fixed filter bar margin bug

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,20 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.css';
-import React, { Component } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import AppRoutes from './AppRoutes';
-import './custom.css';
+import "bootstrap/dist/css/bootstrap.css";
+import React, { Component } from "react";
+import { Route, Routes } from "react-router-dom";
+import AppRoutes from "./AppRoutes";
+import "./custom.css";
+import { Layout } from "./components/Layout";
 
 export default class App extends Component {
   static displayName = App.name;
 
   render() {
     return (
-      <Routes>
-        {AppRoutes.map((route, index) => {
-          const { element, ...rest } = route;
-          return <Route key={index} {...rest} element={element} />;
-        })}
-      </Routes>
+      <>
+        <Layout />
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element} />;
+          })}
+        </Routes>
+      </>
     );
   }
 }
--- ClientApp/src/components/NavMenu.js
+++ ClientApp/src/components/NavMenu.js
@@ -1,45 +1,64 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import './NavMenu.css';
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import "./NavMenu.css";
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true
+      collapsed: true,
     };
   }
 
-  toggleNavbar () {
+  toggleNavbar() {
     this.setState({
-      collapsed: !this.state.collapsed
+      collapsed: !this.state.collapsed,
     });
   }
 
   render() {
     return (
       <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">CSI_Brady</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm bg-brady">
+          <NavbarBrand tag={Link} to="/">
+            <img src="brady-logo.png" alt="Brady logo" height={30} />
+          </NavbarBrand>
+          <NavbarToggler
+            onClick={this.toggleNavbar}
+            className="mr-2 navbar-dark"
+          />
+          <Collapse
+            className="d-sm-inline-flex flex-sm-row-reverse"
+            isOpen={!this.state.collapsed}
+            navbar
+          >
             <ul className="navbar-nav flex-grow">
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
+                <NavLink tag={Link} className="text-white" to="/facilities">
+                  Home
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
+                <NavLink tag={Link} className="text-white" to="/counter">
+                  Profile
+                </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/fetch-data">Fetch data</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/projects">Projects</NavLink>
+                <NavLink tag={Link} className="text-white" to="/fetch-data">
+                  Logout
+                </NavLink>
               </NavItem>
             </ul>
           </Collapse>
