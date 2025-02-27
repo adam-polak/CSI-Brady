7b9f97552b50651b9439e935adcc020e101148ab
Adam <adamjpolak@gmail.com>
2025-02-26 22:59:19 -0600

    more camera functioality

--- ClientApp/src/components/camera/Camera.js
+++ ClientApp/src/components/camera/Camera.js
@@ -1,34 +1,73 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import { Container, Row, Col, Button } from "reactstrap";
 
-export default function Camera({ handleCapture }) {
+export default function Camera() {
   const webcamRef = useRef();
+  const [imgSrc, setImgSrc] = useState(null);
+  const [error, setError] = useState(null);
+
+// 10mb = 100,000,000 bytes, images must be <= this size
+const maxImageSize = 100000000;
 
   function capture() {
-    const imageSrc = webcamRef.current.getScreenshot();
-    handleCapture(imageSrc);
+    setImgSrc(webcamRef.current.getScreenshot());
+  }
+
+  function fileUpload(e) {
+    const file = e.target.files[0];
+    if(imgSrc || !file || file.size > maxImageSize) {
+      setError("Image too large")
+      return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      const base64String = e.target.result;
+      setImgSrc(base64String);
+    }
+
+    reader.onerror = (err) => {
+      setError(err);
+    }
+
+    reader.readAsDataURL(file);
+
+    setImgSrc(file.name);
+  }
+
+  function retry() {
+    setError(null);
+    setImgSrc(null);
   }
 
   return (
     <Container fluid className="p-2" style={{height: "95%"}}>
+      {error ? <h4 className="text-danger">* {error}</h4> : <></>}
       <Row className="d-flex justify-content-center mb-2">
         <Col xs="2" />
         <Col>
-          <input type="file" />
+          {!imgSrc ? <input onChange={(e) => fileUpload(e)} type="file" /> : <></>}
         </Col>
       </Row>
       <Row className="d-flex justify-content-center">
-          <Webcam
-            audio={false}
-            ref={webcamRef}
-            style={{height: "65vh"}}
-            screenshotFormat="image/jpeg"
-            videoConstraints={{ facingMode: "environment" }}
-          />
-          <div className="d-flex justify-content-center">
+          { !imgSrc ?
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              style={error ? {height : "55vh"} : {height: "63vh"}}
+              screenshotFormat="image/jpeg"
+              videoConstraints={{ facingMode: "environment" }}
+            />
+            : <img src={imgSrc} style={{maxHeight: "65vh"}} alt="Capture" />
+          }
+          <div className="d-flex justify-content-center mt-2">
             <Col xs="3">
-              <Button onClick={() => capture()} color="success" style={{height: "4em", width: "8em"}}>Capture</Button>
+              { !imgSrc ?
+                <Button onClick={() => capture()} color="success" style={{height: "4em", width: "8em"}}>Capture</Button>
+                : <Button onClick={() => retry()} color="primary" style={{height: "4em", width: "8em"}}>Retry</Button>
+              }
             </Col>
           </div>
       </Row>
--- ClientApp/src/components/camera/CameraPage.js
+++ ClientApp/src/components/camera/CameraPage.js
@@ -4,18 +4,13 @@ import { Container } from "reactstrap";
 import NavHeader from "../header/NavHeader";
 
 export default function CameraPage() {
-
-  function storePhoto(imgText) {
-    console.log(imgText);
-  }
-
   return (
     <div className="bg-grey" style={{height: "93vh"}}>
       <div style={{width: "100%"}}>
         <NavHeader />
       </div>
       <Container fluid className="text-center p-2" style={{width: "90%"}}>
-        <Camera handleCapture={storePhoto} />
+        <Camera />
       </Container>
     </div>
   );
