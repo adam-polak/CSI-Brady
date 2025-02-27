354f6b8b25ae18e01b776205c1ca29cc5110d707
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-02-27 14:59:31 -0600

    FacilityPage skeleton

---
+++ ClientApp/src/components/facility/AreaEntry.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Card } from 'reactstrap'
+
+export default function AreaEntry () {
+  return (
+    <Card>
+        Area Entry
+    </Card>
+  )
+}
+
---
+++ ClientApp/src/components/facility/FacilityPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import AreaEntry from "./AreaEntry";
+
+export function FacilityPage() {
+  return (
+    <div style={{ height: "93vh" }} className="bg-grey pt-3">
+      <AreaEntry></AreaEntry>
+    </div>
+  );
+}
