6872fb98a3a0f1409c742127a4e39ddd62997cf2
Adam <adamjpolak@gmail.com>
2025-02-27 22:40:56 -0600

    edit image controller to return id of image created

--- DataAccess/Controllers/ImageController.cs
+++ DataAccess/Controllers/ImageController.cs
@@ -1,16 +1,45 @@
+using CSI_Brady.DataAccess.Models;
 using CSI_Brady.DataAccess.Util;
 
 namespace CSI_Brady.DataAccess.Controllers;
 
 public class ImageController : DbController
 {
-    public async Task CreateImage(int areaId, int userId)
+    /*
+        Returns unique image id
+    */
+    public async Task<int> CreateImage(int areaId, int userId)
     {
-        string sql = "INSERT INTO image (areaid, userid)"
-                    + " VALUES (@area, @user);";
-        object[] parameters = { new { area = areaId, user = userId } };
+        int id = await GenerateRandomId();
+        
+        string sql = "INSERT INTO image (id, areaid, userid)"
+                    + " VALUES (@id, @area, @user);";
+        object[] parameters = { new { id = id, area = areaId, user = userId } };
 
         await DoCommandAsync(sql, parameters);
+
+        return id;
+    }
+
+    private async Task<int> GenerateRandomId()
+    {
+        Random rnd = new Random();
+        
+        int id = rnd.Next(0, 999999999);
+        while(await ContainsId(id))
+        {
+            id = rnd.Next(0, 999999999);
+        }
+
+        return id;
+    }
+
+    public async Task<bool> ContainsId(int id)
+    {
+        string sql = "SELECT * FROM IMAGE WHERE id=@id;";
+        object obj = new { id = id };
+
+        return (await DoQueryAsync<ImageModel>(sql, obj)).Count != 0;
     }
 
     public async Task AddProductToImage(int imgId, int productId)
---
+++ DataAccess/Models/ImageModel.cs
@@ -0,0 +1,8 @@
+namespace CSI_Brady.DataAccess.Models;
+
+public class ImageModel
+{
+    public required int Id { get; set; }
+    public required int AreaId { get; set; }
+    public required int UserId { get; set; }
+}
