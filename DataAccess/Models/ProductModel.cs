d1c1e113711c58bfe98ea89a3eefb69c6bdfad46
Adam <adamjpolak@gmail.com>
2025-02-27 21:05:45 -0600

    create product model and violation model

---
+++ DataAccess/Models/ProductModel.cs
@@ -0,0 +1,14 @@
+namespace CSI_Brady.DataAccess.Models;
+
+/**
+    The model to be used on a specific area page
+*/
+public class ProductModel
+{
+    public required int Id { get; set; }
+    public required string Name { get; set; }
+    public required string Link { get; set; }
+    public required string ImgSrc { get; set; }
+    public required int ImageCount { get; set; }
+    public ViolationModel[] violations { get; set; } = [];
+}
