af0d123af24ed2c4a75d3bc00226d37dbbc4e015
Adam <adamjpolak@gmail.com>
2025-02-27 22:07:48 -0600

    create product controller

---
+++ DataAccess/Controllers/ProductController.cs
@@ -0,0 +1,15 @@
+using CSI_Brady.DataAccess.Util;
+
+namespace CSI_Brady.DataAccess.Controllers;
+
+public class ProductController : DbController
+{
+    public async Task CreateProduct(string name, string link, string imgSrc)
+    {
+        string sql = "INSERT INTO product (name, link, imgsrc)"
+                    + " VALUES (@name, @link, @img);";
+        object[] parameters = { new { name = name, link = link, img = imgSrc } };
+
+        await DoCommandAsync(sql, parameters);
+    }
+}
