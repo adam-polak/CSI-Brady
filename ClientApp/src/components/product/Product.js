3926b9cf3bd2cbf4cadf771a8b91123f1519b2f4
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-03-01 17:36:05 -0600

    SubmissionPage and Product comp rework

--- ClientApp/src/components/product/Product.js
+++ ClientApp/src/components/product/Product.js
@@ -1,13 +1,5 @@
-import React, { useState } from "react";
-import {
-  Accordion,
-  AccordionBody,
-  AccordionHeader,
-  AccordionItem,
-  Card,
-  Col,
-  Row,
-} from "reactstrap";
+import React from "react";
+import { Col, Row } from "reactstrap";
 
 /**
  * Product object
@@ -30,57 +22,33 @@ import {
  */
 
 export default function Product({ product }) {
-  const [open, setOpen] = useState("");
-  const toggle = (id) => {
-    if (open === id) {
-      setOpen();
-    } else {
-      setOpen(id);
-    }
-  };
-
   return (
-    <Card className="mb-3 p-3">
-      <Row className="align-items-center">
-        <Col xs="auto">
-          <a href={product.Link} target="_blank" rel="noreferrer" className="text-decoration-none text-dark">
-            <img
-              alt="Product"
-              src={product.ImgSrc}
-              className="img-fluid"
-              style={{ maxHeight: "100px", width: "auto" }}
-            />
-          </a>
-        </Col>
-        <Col>
-          <a href={product.Link} target="_blank" rel="noreferrer" className=" text-dark">
-            <h3>{product.Name}</h3>
-          </a>
-        </Col>
-      </Row>
-      {product.Violations.length !== 0 && <hr />}
-      {product.Violations.map((violation, i) => {
-        return (
-          <>
-            <Row>
-              <Col>
-                <Accordion flush open={open} toggle={toggle}>
-                  <AccordionItem>
-                    <AccordionHeader targetId={"" + i}>
-                      {violation.Name}
-                    </AccordionHeader>
-                    <AccordionBody accordionId={"" + i}>
-                      {violation.Summary}{" "}
-                      <a target="_blank" rel="noreferrer" href={violation.Link}>More Information</a>
-                    </AccordionBody>
-                  </AccordionItem>
-                </Accordion>
-              </Col>
-            </Row>
-            {i !== product.Violations.length - 1 && <hr />}
-          </>
-        );
-      })}
-    </Card>
+    <Row className="align-items-center">
+      <Col xs="auto">
+        <a
+          href={product.Link}
+          target="_blank"
+          rel="noreferrer"
+          className="text-decoration-none text-dark"
+        >
+          <img
+            alt="Product"
+            src={product.ImgSrc}
+            className="img-fluid"
+            style={{ maxHeight: "100px", width: "auto" }}
+          />
+        </a>
+      </Col>
+      <Col>
+        <a
+          href={product.Link}
+          target="_blank"
+          rel="noreferrer"
+          className=" text-dark"
+        >
+          <h3>{product.Name}</h3>
+        </a>
+      </Col>
+    </Row>
   );
 }
