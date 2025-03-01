661bd0901a006f7eb490a2ecec64e459028d9e08
Adam <adamjpolak@gmail.com>
2025-03-01 16:24:14 -0600

    uncomment link

--- Controllers/ImageController.cs
+++ Controllers/ImageController.cs
@@ -70,8 +70,7 @@ public class ImageController : ControllerBase
 
         using HttpClient client = new HttpClient();
         HttpResponseMessage response = await client.PostAsync(
-            "http://localhost:8000/detect",
-            // "https://csifastai.azurewebsites.net/detect", 
+            "https://csifastai.azurewebsites.net/detect", 
             new StringContent(imgBase64)
         );
 
